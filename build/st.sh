3665383362c126c0f1831af9953080e7d09a97d4
Holesch, Simon (BSH) <simon.holesch@bshg.com>
2017-10-25 19:11:40 +0200

    Remap capslock in zshrc
    - fix st download path

--- build/st.sh
+++ build/st.sh
@@ -7,7 +7,7 @@ ST_DIR=$(dirname $SOURCE_DIR)
 
 rm -rf $ST_DIR
 mkdir -p $ST_DIR
-curl http://dl.suckless.org/st/st-$VERSION.tar.gz | tar xzC $ST_DIR
+curl https://dl.suckless.org/st/st-$VERSION.tar.gz | tar xzC $ST_DIR
 cd $SOURCE_DIR
 
 make clean install PREFIX=$HOME/.local
