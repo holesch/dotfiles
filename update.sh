eaad0c31ac56bfe88a9f8492280458a44ab08e97
Simon Holesch <simon@holesch.de>
2018-06-10 16:04:34 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -13,13 +13,17 @@ git subtree pull --prefix vim/pack/user/start/base16-color https://github.com/ch
 git subtree pull --prefix vim/pack/user/start/bitbake https://github.com/siho22/vim-bitbake master --squash
 git subtree pull --prefix vim/pack/user/start/commentary https://github.com/tpope/vim-commentary master --squash
 git subtree pull --prefix vim/pack/user/start/ctrlp https://github.com/ctrlpvim/ctrlp.vim master --squash
+git subtree pull --prefix vim/pack/user/start/dispatch https://github.com/tpope/vim-dispatch master --squash
 git subtree pull --prefix vim/pack/user/start/eunuch https://github.com/tpope/vim-eunuch master --squash
 git subtree pull --prefix vim/pack/user/start/fugitive https://github.com/tpope/vim-fugitive master --squash
 git subtree pull --prefix vim/pack/user/start/gutentags https://github.com/ludovicchabant/vim-gutentags master --squash
+git subtree pull --prefix vim/pack/user/start/lion https://github.com/tommcdo/vim-lion master --squash
 git subtree pull --prefix vim/pack/user/start/neocomplete https://github.com/Shougo/neocomplete.vim master --squash
 git subtree pull --prefix vim/pack/user/start/repeat https://github.com/tpope/vim-repeat master --squash
 git subtree pull --prefix vim/pack/user/start/sensible https://github.com/tpope/vim-sensible master --squash
+git subtree pull --prefix vim/pack/user/start/sneak https://github.com/justinmk/vim-sneak master --squash
 git subtree pull --prefix vim/pack/user/start/surround https://github.com/tpope/vim-surround master --squash
+git subtree pull --prefix vim/pack/user/start/targets https://github.com/wellle/targets.vim master --squash
 git subtree pull --prefix vim/pack/user/start/tmux-navigator https://github.com/christoomey/vim-tmux-navigator master --squash
 git subtree pull --prefix vim/pack/user/start/unimpaired https://github.com/tpope/vim-unimpaired master --squash
 git subtree pull --prefix vim/pack/user/start/vinegar https://github.com/tpope/vim-vinegar master --squash
