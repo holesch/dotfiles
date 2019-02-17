cd051a9a256761ae67f99c1c438a5b2ff679333f
Simon Holesch <simon@holesch.de>
2019-02-17 20:59:33 +0100

    Squashed 'gibo/' changes from a031cfe18..ff9f2f807
    ff9f2f807 Remove git pull --autostash option
    3dba092bd Bump version to 2.1.2
    0bb162716 Remove broken she-bang line
    e149a4518 Improve connectivity with UNIX-Pipeline
    f78a604a6 Add search command
    de5ea614e Tidy up README
    aec41db7e Update gibo.fish
    c3cc28021 Merge pull request #32 from kodybrown/master
    622c13e45 Added support for GIBO_BOILERPLATES envar. Cleaned up the argument handler a bit (more friendly to Windows users). Added suggestions when there is an invalid argument. Added `search expr` command. Added optional parameter to `list` command. Added `--autostash` and `-q` to the git pull command.
    64b727673 Tidy up README
    0f92d4148 Update gibo.bat to v2.0
    3899a48e0 Make fish shell tab-completion script load boilerplate names lazily
    243c8f617 Merge pull request #31 from hana-da/fix_readme
    3305464f9 Fix typo "gibo upgrade" -> "gibo update" in README.md
    619efe808 Tidy up gibo.fish
    d6976b2fd [fish] enable tab-completion of multiple boilerplates
    7a2d6306e [v2] Update bash tab completion script
    9f1157b0a Show usage if dump is called without arguments
    253099023 [v2] Update zsh tab completion script
    3a5ab931c [v2] Update fish tab completion script
    9c27be5b7 [v2] Rename upgrade command to update
    b0327a2ed [v2] Update README
    a3bd54de0 [v2] Switch to subcommand model
    
    git-subtree-dir: gibo
    git-subtree-split: ff9f2f807ecefefb4c93851cee4ab067a7c7988f

--- gibo.bat
+++ gibo.bat
@@ -8,12 +8,14 @@ rem
 rem Change log
 rem v1.0    1-May-2012  First public release
 rem v1.0.01 16-Aug-2014 Added batch file for DOS by Kody Brown ^<thewizard@wasatchwizard.com^>
+rem v2.0.00 11-Jun-2018 Updated to v2; subcommand model by Simon Whitaker ^<sw@netcetera.org^>
+rem v2.0.01 13-Jun-2018 Added optional parameter for list subcommand, added search subcommand, added support for GIBO_BOILERPLATES by Kody Brown ^<thewizard@wasatchwizard.com^>
 
 goto :setup
 
 
 :version
-    echo %basename% 1.0.6 by Simon Whitaker ^<sw@netcetera.org^>
+    echo %basename% 2.1.2 by Simon Whitaker ^<sw@netcetera.org^>
     echo https://github.com/simonwhitaker/gitignore-boilerplates
     goto :eof
 
@@ -30,10 +32,11 @@ goto :setup
     echo     %basename% Python TextMate ^>^> .gitignore
     echo.
     echo Options:
-    echo     -l, --list          List available boilerplates
-    echo     -u, --upgrade       Upgrade list of available boilerplates
-    echo     -h, --help          Display this help text
-    echo     -v, --version       Display current script version
+    echo     list [expr]   List available boilerplates
+    echo     search expr   Search inside boilerplates for expr
+    echo     update        Update list of available boilerplates
+    echo     help          Display this help text
+    echo     version       Display current script version
 
     goto :eof
 
@@ -43,145 +46,135 @@ goto :setup
     set "baseext=%~x0"
     set "basepath=%~dp0"
 
-    set "__quiet="
-    set "__verbose="
-    set "__pause="
+    set "__cloned="
+
+    set "dumping="
 
     set "remote_repo=https://github.com/github/gitignore.git"
-    rem set "local_repo=%UserProfile%\.gitignore-boilerplates"
-    set "local_repo=%AppData%\.gitignore-boilerplates"
+
+    rem Allow using the `GIBO_BOILERPLATES` system envar
+    rem for specifying the boilerplates directory.
+    if defined GIBO_BOILERPLATES set "local_repo=%GIBO_BOILERPLATES%"
+    if not defined GIBO_BOILERPLATES set "local_repo=%AppData%\.gitignore-boilerplates"
 
     rem No args passed in, so show usage.
-    if "%~1"=="" call :usage && exit /B 0
+    if "%~1"=="" call :usage && goto :end
 
 :parse
     rem Parse comand-line options.
 
-    if "%~1"=="" goto :main
-
-    if not defined __quiet (
-        if defined __verbose echo parse^('%~1'^)
-    )
+    if "%~1"=="" goto :end
 
     set a=%~1
-    :: Replace all '--' with '-' (for options)
-    set a=%a:--=-%
-    :: Get the first 2 characters of `a`
-    set a2=%a:~0,2%
 
     rem The batch file's equivalent 'Sanity check' is that any
     rem options (-, --, and / for Windows) are executed then
     rem the batch file exits, ignoring any other commands, such
-    rem as Python
+    rem as Python, etc.
 
-    if /i "%a%"=="/?" call :usage && endlocal && exit /B 0
-    if /i "%a2%"=="-h" call :usage && endlocal && exit /B 0
+    if /i "%a%"=="help"     call :usage         & goto :end
+    if /i "%a%"=="/?"       call :usage         & goto :end
+    if /i "%a%"=="version"  call :version       & goto :end
+    if /i "%a%"=="/v"       call :version       & goto :end
+    if /i "%a%"=="list"     call :list "%~2"    & goto :end
+    if /i "%a%"=="search"   call :search "%~2"  & goto :end
+    if /i "%a%"=="update"   call :update        & goto :end
 
-    if /i "%a2%"=="-q" set "__quiet=1" && shift && goto :parse
-    if /i "%a2%"=="/q" set "__quiet=1" && shift && goto :parse
+    if /i "%a%"=="dump"     set "dumping=1"     & shift & goto :parse
 
-    if /i "%a%"=="-verbose" set "__verbose=1" && shift && goto :parse
-    if /i "%a%"=="/verbose" set "__verbose=1" && shift && goto :parse
+    if defined dumping      call :dump "%a%"    & shift & goto :parse
 
-    if /i "%a2%"=="-p" set "__pause=1" && shift && goto :parse
-    if /i "%a2%"=="/p" set "__pause=1" && shift && goto :parse
-
-    rem rem Takes the next argument also (calls shift twice)..
-    rem if /i "%a2%"=="-repo" set "remote_repo=%~2" && shift && shift && goto :parse
-    rem if /i "%a2%"=="/repo" set "remote_repo=%~2" && shift && shift && goto :parse
-
-    if /i "%a2%"=="-v" call :version && exit /B 0
-    if /i "%a2%"=="/v" call :version && exit /B 0
-
-    if /i "%a2%"=="-l" call :list && exit /B 0
-    if /i "%a2%"=="/l" call :list && exit /B 0
-
-    if /i "%a2%"=="-u" call :upgrade && exit /B 0
-    if /i "%a2%"=="/u" call :upgrade && exit /B 0
-
-    call :dump "%a%"
-
-    shift
-    goto :parse
-
-:main
-    if not defined __quiet (
-        if defined __verbose echo main^(^)
-    )
-    if defined __pause pause
+    goto :invalid_argument "%a%"
 
 :end
     @endlocal && exit /B 0
 
 
-:clone
-    if not defined __quiet (
-        if defined __verbose echo clone^(^)
+:invalid_argument "arg"
+    echo Invalid argument: %~1
+    echo Did you mean:
+
+    rem Is there a .gitignore file?
+    set "_foundfile="
+    if exist "%local_repo%\%~1.gitignore" set "_foundfile=yes"
+    if exist "%local_repo%\Global\%~1.gitignore" set "_foundfile=yes"
+    if defined _foundfile (
+        echo     `%basename% dump %*`
+        echo     `%basename% list %*`
     )
 
-    if not "%~1"=="--silently" (
-        set "opt=-q"
-        if not defined __quiet (
-            echo Cloning %remote_repo% to %local_repo%
-        )
-    )
+    rem Did the user mean to search within .gitignore files?
+    echo     `%basename% search %*`
 
+    endlocal && exit /B 1
+
+:clone [--silently]
+    if "%~1"=="--silently" ( set "opt=-q" ) else ( set "opt=" )
     git clone %opt% "%remote_repo%" "%local_repo%"
-
     goto :eof
 
 
-:init
-    if not defined __quiet (
-        if defined __verbose echo init^(^)
-    )
-
-    if not exist "%local_repo%\.git" call :clone "%~1"
+:init [--silently]
+    if not exist "%local_repo%\.git" set "__cloned=yes" && call :clone "%~1"
     goto :eof
 
 
 :list
-    if not defined __quiet (
-        if defined __verbose echo list^(^)
-    )
-
     call :init
 
     echo === Languages ===
     echo.
-    for /f %%G in ('dir /b /on "%local_repo%\*.gitignore"') do (
+    for /f %%G in ('dir /b /on "%local_repo%\*%~1*.gitignore"') do (
         echo %%~nG
     )
 
     echo.
     echo === Global ===
     echo.
-    for /f %%G in ('dir /b /on "%local_repo%\Global\*.gitignore"') do (
+    for /f %%G in ('dir /b /on "%local_repo%\Global\*%~1*.gitignore"') do (
         echo %%~nG
     )
 
     goto :eof
 
+:search
+    if "%~1"=="" echo %basename%: missing search expr.. && goto :eof
 
-:upgrade
-    if not defined __quiet (
-        if defined __verbose echo upgrade^(^)
-    )
+    call :init
 
-    if not exist "%local_repo%\.git" call :clone && goto :eof
+    rem `findstr` options:
+    rem   /R         Uses search strings as regular expressions.
+    rem   /S         Searches for matching files in the current directory and all
+    rem              subdirectories.
+    rem   /I         Specifies that the search is not to be case-sensitive.
+    rem   /N         Prints the line number before each line that matches.
+    rem   /P         Skip files with non-printable characters.
+    rem   /A:attr    Specifies color attribute with two hex digits. See "color /?"
+    rem   strings    Text to be searched for.
+    rem   [drive:][path]filename
+    rem              Specifies a file or files to search.
 
     pushd "%local_repo%"
-    git pull --ff origin master
+    findstr /S /R /I /N /P /A:03 "%~1" *.gitignore
     popd
+
+    goto :eof
+
+:update
+    call :init
+
+    rem If the repo was just cloned, don't perform a `pull`
+    if not defined __cloned (
+        echo updating..
+        pushd "%local_repo%"
+        git pull -q --ff origin master
+        popd
+    )
 
     goto :eof
 
 
 :dump
-    if not defined __quiet (
-        if defined __verbose echo dump^('%~1'^)
-    )
-
     call :init --silently
 
     set "language_file=%local_repo%\%~1.gitignore"
