8d574ca0c71b86182dd29c18ccd5df35b55ed1ef
Simon Holesch <simon@holesch.de>
2019-03-22 18:51:41 +0100

    Add colortest.sh

---
+++ colortest.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for i in $(seq 1 255) ; do
+    printf "\x1b[48;5;%sm%8d\e[0m " "$i" "$i"
+    if (( i == 15 )) || (( i > 15 )) && (( (i-15) % 6 == 0 )); then
+        printf "\n";
+    fi
+done
