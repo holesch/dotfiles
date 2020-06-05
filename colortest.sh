10896a69d7ac7bbe468302db7f7dfbba5dfe6e33
Holesch, Simon (BSH) <simon.holesch@bshg.com>
2020-06-05 18:32:08 +0200

    Fix colortest

--- colortest.sh
+++ colortest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(seq 1 255) ; do
+for i in $(seq 0 255) ; do
     printf "\x1b[48;5;%sm%8d\e[0m " "$i" "$i"
     if (( i == 15 )) || (( i > 15 )) && (( (i-15) % 6 == 0 )); then
         printf "\n";
