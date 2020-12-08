e90bf383bcc410046b586f34bce4990c1cce2974
Holesch, Simon (BSH) <simon.holesch@bshg.com>
2020-12-08 12:22:33 +0100

    Plantuml: Disable X

--- vim/after/ftplugin/plantuml-vim.sh
+++ vim/after/ftplugin/plantuml-vim.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-plantuml -tsvg -verbose -failfast2 -o /tmp/plantuml-vim "$1"
+DISPLAY= plantuml -tsvg -verbose -failfast2 -o /tmp/plantuml-vim "$1"
 mv "/tmp/plantuml-vim/$(basename "$2")" "$2"
