33071d8b8f221a68d4ba787c47344d4b8d7ae0f3
Holesch, Simon (BSH) <simon.holesch@bshg.com>
2021-11-03 15:03:52 +0100

    fzy: Use upstream with config tweaks

--- build/fzy.sh
+++ build/fzy.sh
@@ -3,14 +3,23 @@ set -e
 
 src="$HOME/.cache/siho-build/fzy"
 
-if [ ! -d $src ]; then
-    git clone https://github.com/siho22/fzy $src
+if [ ! -d "$src" ]; then
+    git clone https://github.com/jhawthorn/fzy "$src"
 fi
 
-cd $src
+cd "$src"
 git reset HEAD --hard
-git pull
+git fetch
+git checkout 395a2534aca4a704da7501c5e79268420e41d174
+
+make config.h
+awk '
+/SCORE_GAP_INNER/ {$3 = "-0.03"}
+/SCORE_MATCH_DOT/ {$3 = "0.8"}
+{print}
+' 'config.h' > 'config.h.tmp'
+mv config.h.tmp config.h
 
 make
 strip fzy
-make PREFIX=$HOME/.local install
+make PREFIX="$HOME/.local" install
