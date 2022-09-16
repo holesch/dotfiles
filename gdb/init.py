81ccacb5a37004d0346c844ac03a355c3960d6c1
Holesch, Simon (BSH) <simon.holesch@bshg.com>
2022-09-16 18:19:52 +0200

    gdb: Fix readline
    Escape sequences need to be enclosed with `\[` and `\]` so that readline
    can calculate the cursor position correctly.

--- gdb/init.py
+++ gdb/init.py
@@ -12,7 +12,7 @@ def prompt(current):
         try:
             frame = gdb.selected_frame()
             function = frame.function()
-            location = " \033[1;39m@\033[1;32m "
+            location = " \[\033[1;39m\]@\[\033[1;32m\] "
             if function:
                 location += function.print_name
             else:
@@ -23,6 +23,6 @@ def prompt(current):
         prog_name = "gdb"
         location = ""
 
-    return '\033[1;32m{}{} \033[1;31m>\033[0m '.format(prog_name, location)
+    return gdb.prompt.substitute_prompt('\[\033[1;32m\]{}{} \[\033[1;31m\]>\[\033[0m\] '.format(prog_name, location))
 
 gdb.prompt_hook = prompt
